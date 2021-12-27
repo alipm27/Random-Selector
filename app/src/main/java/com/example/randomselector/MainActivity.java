b82c6c15ae2283cc1104487e6f5bbf7463a6e887
alipm27 <alipirvandmori@gmail.com>
2021-12-27 18:04:55 +0330

    init commit

--- app/src/main/java/com/example/randomselector/MainActivity.java
+++ app/src/main/java/com/example/randomselector/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.randomselector;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.randomselector.adapter.TabsAdapter;
+import com.example.randomselector.databinding.ActivityMainBinding;
+import com.example.randomselector.fragments.CategoryFragment;
+import com.example.randomselector.fragments.HomeFragment;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        List<Fragment> fragments=new ArrayList<>();
+        fragments.add(new CategoryFragment());
+        fragments.add(new HomeFragment());
+
+        binding.pager.setAdapter(new TabsAdapter(MainActivity.this,fragments));
+
+
+        binding.bottomTabs.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.item_category:
+                        binding.pager.setCurrentItem(0);
+                        binding.bottomTabs.getMenu().findItem(R.id.item_category).setChecked(true);
+                        break;
+                    case R.id.item_home:
+                        binding.pager.setCurrentItem(1);
+                        binding.bottomTabs.getMenu().findItem(R.id.item_home).setChecked(true);
+                        break;
+                }
+                return false;
+            }
+        });
+
+        binding.pager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                switch (position){
+                    case 0:
+                        binding.bottomTabs.getMenu().findItem(R.id.item_category).setChecked(true);
+                        break;
+                    case 1:
+                        binding.bottomTabs.getMenu().findItem(R.id.item_home).setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/randomselector/adapter/TabsAdapter.java
+++ app/src/main/java/com/example/randomselector/adapter/TabsAdapter.java
@@ -1,4 +1,27 @@
 package com.example.randomselector.adapter;
 
-public class TabsAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.List;
+
+public class TabsAdapter extends FragmentStateAdapter {
+    List<Fragment> fragments;
+    public TabsAdapter(@NonNull FragmentActivity fragmentActivity, List<Fragment> fragmentList) {
+        super(fragmentActivity);
+        fragments=fragmentList;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragments.size();
+    }
 }
